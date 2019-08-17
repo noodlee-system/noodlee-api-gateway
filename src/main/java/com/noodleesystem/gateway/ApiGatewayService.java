c7297ece458846ce1944907dc6f2353c69da3ec8
SkrzypMajster <skrzypmajster@gmail.com>
2019-08-18 00:01:50 +0200

    Install and configure Zuul proxy

--- src/main/java/com/noodleesystem/gateway/ApiGatewayService.java
+++ src/main/java/com/noodleesystem/gateway/ApiGatewayService.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import serilogj.Log;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
 public class ApiGatewayService {
     public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayService.class, args);
